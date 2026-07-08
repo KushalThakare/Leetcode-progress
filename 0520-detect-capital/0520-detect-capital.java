c230df7bd00ee61a7ac01585d220d72618ac9231
Kushal <kushalthakare775@gmail.com>
2026-07-08 20:21:48 +0530

    Time: 1 ms (90.34%), Space: 43.4 MB (13.01%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,15 +1,17 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-
-        if(word.equals(word.toUpperCase()) || word.equals(word.toLowerCase())){
-            return true;
+        int capc =0;
+        for(int i=0; i<word.length();i++){
+            char a = word.charAt(i);
+            if(a>='A' && a<='Z'){
+                capc++;
+            }
         }
 
-        if(word.equals(word.substring(0,1).toUpperCase() + word.substring(1).toLowerCase())){
+        if(word.length()==capc||capc==0||(capc==1&&(word.charAt(0)>='A'&&word.charAt(0)<='Z'))){
             return true;
         }
 
         return false;
-        
     }
 }
