b14316355c515d76128ce365fa01bf310767d547
Kushal <kushalthakare775@gmail.com>
2026-07-21 00:41:25 +0530

    Time: 2 ms (66.09%), Space: 43.3 MB (36.15%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,15 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int sums = 0;
+        int sumt = 0;
+
+        for(int i = 0; i<s.length();i++){
+            sums+= (int) (s.charAt(i));
+        }
+        for(int i = 0; i<t.length();i++){
+            sumt+= (int) (t.charAt(i));
+        }
+
+        return (char) (sumt - sums);
+    }
+}
