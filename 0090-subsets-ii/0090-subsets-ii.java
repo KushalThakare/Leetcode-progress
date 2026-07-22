cca2a44bed53017a489c1cfb8e252dff11c40428
Kushal <kushalthakare775@gmail.com>
2026-07-22 15:18:36 +0530

    Time: 3 ms (56.39%), Space: 44.9 MB (77.47%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+     static void solve(int[] nums, int index, List<Integer> output, List<List<Integer>> ans){
+        //base case
+        if(index>=nums.length){
+            //subsequence is ready
+            ans.add(new ArrayList<>(output));
+            return;
+        }
+
+        //include-exclude
+         int curValue = nums[index];
+        //include
+        output.add(curValue);
+        solve(nums, index+1, output, ans);
+        //backtracking step: call se waps jate hue we have to remove the curvalue we added in the output
+        //so output me se curValue remove;
+        output.remove(output.size()-1);
+
+        //exclude
+        while(index+1<nums.length && nums[index]==nums[index+1]){
+            index++;
+        }
+        solve(nums, index+1, output, ans);
+
+       return;
+
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
+        solve(nums, 0, output, ans);
+        return ans;
+    }
+}
