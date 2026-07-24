9fd29c050876140305441994d92b2e4392dd420f
Kushal <kushalthakare775@gmail.com>
2026-07-24 14:28:37 +0530

    Time: 0 ms (100%), Space: 43.4 MB (77.47%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,38 +1,45 @@
 class Solution {
-    public void solve(List<String> ans, List<String> req, String out, int ind){
+
+    public void solve(List<String> ans, List<String> req, StringBuilder out, int ind){
         // base case
         if(ind == req.size()){
-            ans.add(new String(out));
+            ans.add(out.toString());
             return;
         }
 
-        //current index pr req list me jao
-        for(int i=0; i<req.get(ind).length(); i++){
+        // current index pr req list me jao
+        for(int i = 0; i < req.get(ind).length(); i++){
+
             // ek character select krke add kro...then move on to next string
-            out = out + req.get(ind).charAt(i);
-            solve(ans, req, out, ind+1);
-            out = out.substring(0, out.length()-1);
+            out.append(req.get(ind).charAt(i));
+
+            solve(ans, req, out, ind + 1);
+
+            out.deleteCharAt(out.length() - 1);
         }
-
-        //index badhake call kro
-        return;
-
     }
+
     public List<String> letterCombinations(String digits) {
-        
+
         List<String> ans = new ArrayList<>();
 
-        if(digits.length()==0){
-           return ans;
+        if(digits.length() == 0){
+            return ans;
         }
-        String[] maps = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+        String[] maps = {
+            "abc", "def", "ghi", "jkl",
+            "mno", "pqrs", "tuv", "wxyz"
+        };
 
         List<String> req = new ArrayList<>();
-        for(int i=0; i<digits.length(); i++){
-            req.add(maps[(int) (digits.charAt(i)-'0'-2)]);
+
+        for(int i = 0; i < digits.length(); i++){
+            req.add(maps[digits.charAt(i) - '2']);
         }
 
-        solve(ans, req, "", 0);
-    return ans;
+        solve(ans, req, new StringBuilder(), 0);
+
+        return ans;
     }
 }
