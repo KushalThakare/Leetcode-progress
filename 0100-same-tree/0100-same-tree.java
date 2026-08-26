7dca9c090fb2ad00f121eaeb099413481ef358d3
Kushal <kushalthakare775@gmail.com>
2026-08-27 00:01:27 +0530

    Time: 0 ms (100%), Space: 43.1 MB (15.63%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,26 +14,18 @@
  * }
  */
 class Solution {
-
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        //ek bhi value same nhi nikla to false return kro
-        if(p==null && q==null){
+
+        if (p == null && q == null)
             return true;
-        }
-        else if(p ==null||q==null){
+
+        if (p == null || q == null)
             return false;
-        }
 
-        if(p.val!=q.val){
+        if (p.val != q.val)
             return false;
-        }
 
-        // check right k values
-        boolean left = isSameTree(p.left, q.left);
-        //check left k values
-        boolean right = isSameTree(p.right, q.right);
-
-        return left && right;
-        
+        return isSameTree(p.left, q.left) &&
+               isSameTree(p.right, q.right);
     }
 }
